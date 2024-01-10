26cf7941981099935d02cdaa4f8f63e703ea0018
PauloLandes <landescrew.scoot@gmail.com>
2024-01-10 17:01:35 +0100

    UP the dedicated char test file

--- tests/test_char.c
+++ tests/test_char.c
@@ -219,3 +219,69 @@ Test(test_eq_char_with_null_struct, test17, .init = redirect_all_std, .signal = 
     delete(character2);
     cr_assert_stderr_eq_str("Null pointer passed");
 }
+
+/* Test the greater than of char */
+
+Test(test_gt_char, test18)
+{
+    char_class_t *character = new(Char, 'a');
+    char_class_t *character2 = new(Char, 'b');
+    bool character3 = gt(character, character2);
+    cr_assert_eq(character3, false);
+    delete(character);
+    delete(character2);
+}
+
+Test(test_gt_char_with_null_value, test19, .signal = SIGABRT)
+{
+    char_class_t *character = new(Char, 'a');
+    char_class_t *character2 = new(NULL, 'b');
+    bool character3 = gt(character, character2);
+    (void)character3;
+    delete(character);
+    delete(character2);
+}
+
+Test(test_gt_char_with_null_struct, test20, .init = redirect_all_std, .signal = SIGABRT)
+{
+    char_class_t *character = new(Char, 'a');
+    char_class_t *character2 = new(NULL, 'b');
+    bool character3 = gt(character, character2);
+    (void)character3;
+    delete(character);
+    delete(character2);
+    cr_assert_stderr_eq_str("Null pointer passed");
+}
+
+/* Test the lower than of char */
+
+Test(test_lt_char, test21)
+{
+    char_class_t *character = new(Char, 'a');
+    char_class_t *character2 = new(Char, 'b');
+    bool character3 = lt(character, character2);
+    cr_assert_eq(character3, true);
+    delete(character);
+    delete(character2);
+}
+
+Test(test_lt_char_with_null_value, test22, .signal = SIGABRT)
+{
+    char_class_t *character = new(Char, 'a');
+    char_class_t *character2 = new(NULL, 'b');
+    bool character3 = lt(character, character2);
+    (void)character3;
+    delete(character);
+    delete(character2);
+}
+
+Test(test_lt_char_with_null_struct, test23, .init = redirect_all_std, .signal = SIGABRT)
+{
+    char_class_t *character = new(Char, 'a');
+    char_class_t *character2 = new(NULL, 'b');
+    bool character3 = lt(character, character2);
+    (void)character3;
+    delete(character);
+    delete(character2);
+    cr_assert_stderr_eq_str("Null pointer passed");
+}
