e03a1d58e1289d198f1b9f69b764bf9a87671518
PauloLandes <landescrew.scoot@gmail.com>
2024-01-10 13:54:54 +0100

    UP dedicated vertex functions test file is almost fully tested

--- tests/test_vertex.c
+++ tests/test_vertex.c
@@ -135,3 +135,111 @@ Test (test_vertex_sub_with_null_value1, test5, .init = redirect_all_std, .signal
     vertex_class_t *vertex2 = new(NULL, 4, 5, 6);
     subtraction(vertex1, NULL);
 }
+
+Test (test_va_vertex_mul, test_6) {
+    vertex_class_t *vertex1 = new(Vertex, 1, 2, 3);
+    vertex_class_t *vertex2 = new(Vertex, 4, 5, 6);
+    vertex_class_t *vertex3 = multiplication(vertex1, vertex2);
+    cr_assert_eq(vertex3->x, 4);
+    cr_assert_eq(vertex3->y, 10);
+    cr_assert_eq(vertex3->z, 18);
+    delete(vertex1);
+    delete(vertex2);
+    delete(vertex3);
+}
+
+Test (test_va_vertex_mul_null, test_6, .signal=SIGABRT) {
+    vertex_class_t *vertex1 = new(NULL, 1, 2, 3);
+    vertex_class_t *vertex2 = new(NULL, 4, 5, 6);
+    vertex_class_t *vertex3 = multiplication(vertex1, vertex2);
+    cr_assert_null(vertex3);
+    delete(vertex1);
+    delete(vertex2);
+    delete(vertex3);
+}
+
+Test (test_va_vertex_mul_null_values, test_6, .init = redirect_all_std, .signal=SIGABRT) {
+    vertex_class_t *vertex1 = va_new(NULL, NULL);
+    vertex_class_t *vertex2 = va_new(NULL, NULL);
+    vertex_class_t *vertex3 = multiplication(vertex1, vertex2);
+    cr_assert_stderr_eq_str("Null pointer passed");
+    delete(vertex1);
+    delete(vertex2);
+    delete(vertex3);
+}
+
+Test (test_vertex_with_null_value2, test5, .init = redirect_all_std, .signal=SIGABRT) {
+    vertex_class_t *vertex1 = new(Vertex, 1, 2, 3);
+    vertex_class_t *vertex2 = new(NULL, 4, 5, 6);
+    multiplication(vertex1, NULL);
+}
+
+Test (test_vertex_div, test7) {
+    vertex_class_t *vertex1 = new(Vertex, 1, 2, 3);
+    vertex_class_t *vertex2 = new(Vertex, 4, 5, 6);
+    vertex_class_t *vertex3 = division(vertex1, vertex2);
+    cr_assert_eq(vertex3->x, 0);
+    cr_assert_eq(vertex3->y, 0);
+    cr_assert_eq(vertex3->z, 0);
+    delete(vertex1);
+    delete(vertex2);
+    delete(vertex3);
+}
+
+Test (test_vertex_div_null, test7, .signal=SIGABRT) {
+    vertex_class_t *vertex1 = new(NULL, 1, 2, 3);
+    vertex_class_t *vertex2 = new(NULL, 4, 5, 6);
+    vertex_class_t *vertex3 = division(vertex1, vertex2);
+    cr_assert_null(vertex3);
+    delete(vertex1);
+    delete(vertex2);
+    delete(vertex3);
+}
+
+Test (test_vertex_div_null_values, test7, .init = redirect_all_std, .signal=SIGABRT) {
+    vertex_class_t *vertex1 = va_new(NULL, NULL);
+    vertex_class_t *vertex2 = va_new(NULL, NULL);
+    vertex_class_t *vertex3 = division(vertex1, vertex2);
+    cr_assert_stderr_eq_str("Null pointer passed");
+    delete(vertex1);
+    delete(vertex2);
+    delete(vertex3);
+}
+
+Test (test_vertex_div_with_null_value1, test7, .init = redirect_all_std, .signal=SIGABRT) {
+    vertex_class_t *vertex1 = new(Vertex, 1, 2, 3);
+    vertex_class_t *vertex2 = new(NULL, 4, 5, 6);
+    division(vertex1, NULL);
+}
+
+Test (test_vertex_eq, test8) {
+    vertex_class_t *vertex1 = new(Vertex, 1, 2, 3);
+    vertex_class_t *vertex2 = new(Vertex, 1, 2, 3);
+    bool res = eq(vertex1, vertex2);
+    cr_assert_eq(res, true);
+    delete(vertex1);
+    delete(vertex2);
+}
+
+Test (test_vertex_eq_null, test8, .signal=SIGABRT) {
+    vertex_class_t *vertex1 = new(NULL, 1, 2, 3);
+    vertex_class_t *vertex2 = new(NULL, 1, 2, 3);
+    bool res = eq(vertex1, vertex2);
+    cr_assert_null(res);
+    delete(vertex1);
+    delete(vertex2);
+}
+
+Test (test_vertex_eq_null_values, test8, .init = redirect_all_std, .signal=SIGABRT) {
+    vertex_class_t *vertex1 = va_new(NULL, NULL);
+    vertex_class_t *vertex2 = va_new(NULL, NULL);
+    bool res = eq(vertex1, vertex2);
+    cr_assert_stderr_eq_str("Null pointer passed");
+    delete(vertex1);
+    delete(vertex2);
+}
+
+Test (test_vertex_eq_with_null_value1, test8, .init = redirect_all_std, .signal=SIGABRT) {
+    vertex_class_t *vertex1 = new(Vertex, 1, 2, 3);
+    bool res = eq(vertex1, NULL);
+}
